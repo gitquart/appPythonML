1d5a8037362430ce11fa9a7ebc44df8f73167c8b
Ulyses Rico <ulyses@quart.com.mx>
2020-09-08 10:34:32 -0500

    ...

--- topicModeling.py
+++ topicModeling.py
@@ -7,21 +7,29 @@ import MLfunctions as mlf
 from sklearn.decomposition import NMF
 import random
 
+"""
+Topic Modeling
+-Latent Dirichlet Allocation (LDA)
+-Non-Negative Matrix Factorization (NMF)
+
+It can be used with TD IDF or Normal Vector
+"""
 
 pathtohere=os.getcwd()
 
 
 def main():
+    print('1.LDA, 2.NMF')
     lsRes=[]
     lsRes=mlf.get_TFIDF()
     tfidf=lsRes[0]
     lsDocs=lsRes[1]
     tfidf_matrix=tfidf.fit_transform(lsDocs)
-    nmf = NMF(n_components=5, random_state=42)
+    nmf = NMF(n_components=2,init='random',random_state=0)
     nmf.fit(tfidf_matrix)
     for i,topic in enumerate(nmf.components_):
-        print(f'Top 10 words for topic #{i}:')
-        print([tfidf.get_feature_names()[i] for i in topic.argsort()[-10:]])
+        print(f'Top 100 words for topic #{i}:')
+        print([tfidf.get_feature_names()[i] for i in topic.argsort()[-100:]])
         print('\n')  
 
 
